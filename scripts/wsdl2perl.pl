9bf0d5b9007d9f180fbc4d5f345d3cf631ba4d14
Ken Prows <ken@xev.net>
2014-12-06 21:15:13 -0500

    better wsdl parsing

--- scripts/wsdl2perl.pl
+++ scripts/wsdl2perl.pl
@@ -4,6 +4,8 @@ use strict;
 use LWP::Simple;
 use File::Slurp;
 use XML::LibXML;
+use URI;
+
 use Getopt::Long;
 
 my $PACKAGE_PREFIX;
@@ -23,34 +25,39 @@ elsif (-f $WSDL_URI)
 {
     $wsdl_string = read_file($WSDL_URI);
 }
+else
+{
+    die "cannot load wsdl";
+}
 
 # remove namespaces to make parsing easier
 # also, this will work with broken wsld files that do not declare namespaces correctly
+# yea, this is a hack...
 $wsdl_string =~ s{( < (?:\s*/\s*)? ) \w+\: (\w+)}{$1$2}xg;
+$wsdl_string =~ s{ \s \w+: (\w+=")  }{ $1}xg;
 
 my $wsdl_dom = XML::LibXML->load_xml(
     string => (\$wsdl_string),
 );
-
-my $has_import = $wsdl_dom->findvalue('//import/@schemaLocation');
-if ($has_import)
-{
-    die "this script does not work with xsd:import elements";
-}
-
-# my $wsdl_dom = XML::LibXML->load_xml(
-#     location => $WSDL_URI,
-# );
-
 my $wsdl_root = $wsdl_dom->documentElement;
+#print $wsdl_root->toString(1) . "\n";
 
 unless ($PACKAGE_PREFIX)
 {
     my $name = $wsdl_root->getAttribute('name');
+    die "cannot find the service name in the WSDL, you must pass --package_prefix for this service" unless $name;
     $PACKAGE_PREFIX = 'SOAP::Sanity::' . $name;
 }
-
 print "package prefix will be: $PACKAGE_PREFIX\n";
+
+# remove root attributes...LibXML is finicky
+$wsdl_string =~ s{(<\w+) [^>]+}{$1};
+
+my $has_import = $wsdl_dom->findvalue('//import/@schemaLocation');
+if ($has_import)
+{
+    die "this script does not work with import elements";
+}
 
 # Services are defined using six major elements:
 # 
@@ -65,12 +72,16 @@ print "package prefix will be: $PACKAGE_PREFIX\n";
 # Load service so we know where to POST to
 #
 my $service_uri = $wsdl_root->findvalue('service/port/address/@location');
+die "cannot determine service uri" unless $service_uri;
 print "service is at: $service_uri\n";
+
+my %TYPES;
 
 #
 # Load types
 #
 my $types_nodes = $wsdl_root->findnodes('types');
+die "cannot load types" unless $types_nodes;
 foreach my $type_node (@$types_nodes)
 {
     print "found types element\n";
@@ -82,34 +93,116 @@ foreach my $type_node (@$types_nodes)
         
         print "found schema with a target namespace of: $target_namespace\n";
         
-        my $complex_type_nodes = $schema_node->findnodes('complexType');
-        foreach my $complex_type_node (@$complex_type_nodes)
+        TYPE_NODES: foreach my $type_node ( $schema_node->findnodes('./*') )
         {
-            my $name = $complex_type_node->getAttribute('name');
+            my $name = $type_node->getAttribute('name');
             
-            print "found complexType: $name\n";
-            
-            my $sequence_nodes = $complex_type_node->findnodes('sequence');
-            foreach my $sequence_node (@$sequence_nodes)
+            unless ($name)
             {
-                my $element_nodes = $sequence_node->findnodes('element');
-                foreach my $element_node (@$element_nodes)
-                {
-                    my $name = $element_node->getAttribute('name');
-                    my $type = remove_namespace( $element_node->getAttribute('type') );
-                    my $min_occurs = $element_node->getAttribute('minOccurs') || 1;
-
-                    my $nillable = $element_node->getAttribute('nillable') || 'false';
-                    $nillable = $nillable eq 'false' ? 0 : 1;
-
-                    print "\telement: $name, type: $type, min_occurs: $min_occurs, nillable?: $nillable\n";
-                }
+                next TYPE_NODES;
             }
             
-            # http://www.w3schools.com/schema/el_complexcontent.asp
-            my $complex_content_nodes = $complex_type_node->findnodes('sequence');
+            $TYPES{$name} = {
+                name => $name,
+                fields => parse_type($type_node),
+            };
         }
     }
+}
+
+sub parse_type
+{
+    my ($type_node) = @_;
+    
+    my @fields;
+    
+    my $node_name = $type_node->nodeName;
+    my $name = remove_namespace( $type_node->getAttribute('name') ) || "";
+    my $type = remove_namespace( $type_node->getAttribute('type') ) || "";
+    
+    print "\n";
+    print "**********************************************************************\n";
+    print "PARSING TYPE\n\tnode_name: $node_name\n\tname: $name\n\ttype: $type\n";
+    print "**********************************************************************\n";
+    
+    if ($type)
+    {
+        push(@fields, parse_field($type_node));
+    }
+    else
+    {
+        foreach my $node ( $type_node->findnodes('./*') )
+        {
+            my $node_name = $node->nodeName;
+            print "Parsing node: $node_name\n";
+
+            if ( $node_name eq 'sequence' )
+            {
+                push(@fields, parse_sequence($node));
+            }
+            elsif ( $node_name eq 'complexContent' )
+            {
+                foreach my $extension_node ( $node->findnodes('extension') )
+                {
+                    my $base_name = remove_namespace( $extension_node->findvalue('@base') );
+                    
+                    if ($base_name)
+                    {
+                        my ($base_node) = $wsdl_root->findnodes(q|//complexType[@name='| . $base_name . q|']|);
+                        if ($base_node)
+                        {
+                            print "Loaded fields from the base type \"$base_name\":\n";
+                            my ($base_sequence) = $base_node->findnodes('sequence');
+                            push(@fields, parse_sequence($base_sequence));
+                        }
+                        else
+                        {
+                            die "cannot load extension with base name: $base_name";
+                        }
+                        
+                        print "Extending \"$base_name\" type with these fields:\n";
+                        my ($additional_sequence) = $extension_node->findnodes('sequence');
+                        push(@fields, parse_sequence($additional_sequence));
+                    }
+                }
+            }
+        }
+    }
+    
+    return @fields;
+}
+
+# returns an array of fields
+sub parse_sequence
+{
+    my ($sequence_node) = @_;
+    
+    my @fields;
+    
+    foreach my $element_node ( $sequence_node->findnodes('element') )
+    {
+        push(@fields, parse_field($element_node));
+    }
+    
+    return @fields;
+}
+
+sub parse_field
+{
+    my ($element_node) = @_;
+    
+    my $field = {};
+
+    $field->{name} = $element_node->getAttribute('name');
+    $field->{type} = remove_namespace( $element_node->getAttribute('type') );
+    $field->{min_occurs} = $element_node->getAttribute('minOccurs') || 1;
+
+    my $nillable = $element_node->getAttribute('nillable') || 'false';
+    $field->{nillable} = $nillable eq 'false' ? 0 : 1;
+
+    print "\telement: $field->{name}, type: $field->{type}, min_occurs: $field->{min_occurs}, nillable?: $field->{nillable}\n";
+    
+    return $field;
 }
 
 #
@@ -126,20 +219,45 @@ foreach my $port_type_node (@$port_type_nodes)
     foreach my $operation_node (@$operation_nodes)
     {
         my $name = $operation_node->getAttribute('name');
-        print "\tfound method: $name\n";
+        
+        my $input = remove_namespace( $operation_node->findvalue('input/@message') );
+        # TODO can there be multiple parts?
+        my ($input_type) = remove_namespace( $wsdl_root->findvalue(q|//message[@name='| . $input . q|']/part/@element|) );
+        unless ($input_type)
+        {
+            ($input_type) = remove_namespace( $wsdl_root->findvalue(q|//message[@name='| . $input . q|']/part/@type|) );
+        }
+        
+        my $output = remove_namespace( $operation_node->findvalue('output/@message') );
+        # TODO can there be multiple parts?
+        my ($output_type) = remove_namespace( $wsdl_root->findvalue(q|//message[@name='| . $output . q|']/part/@element|) );
+        unless ($output_type)
+        {
+            ($output_type) = remove_namespace( $wsdl_root->findvalue(q|//message[@name='| . $output . q|']/part/@type|) );
+        }
+        
+        print "\tfound method: $name\n\t\tinput: $input_type ($input)\n\t\toutput: $output_type ($output)\n";
     }
 }
 
-sub create_module
-{
-    my (%args) = @_;
-    
-    
-}
+# sub create_module
+# {
+#     my (%args) = @_;
+#     
+#     
+# }
 
 sub remove_namespace
 {
     my ($name) = @_;
+    return "" unless $name;
     $name =~ s/^\w+://;
     return $name;
 }
+
+# sub get_children
+# {
+#     my ($parent_node, $child_name) = @_;
+#     my $children = $parent_node->findnodes($child_name);
+#     return wantarray ? @$children : $children;
+# }
